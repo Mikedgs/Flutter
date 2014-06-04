b7c6cf6bd88239d65649e9d33140bf103b3f9357
Ninjah45 <Ben.tehira@gmail.com>
2014-06-05 10:43:53 +1200

    Shell added

---
+++ SharpTwit/SharpTwit.cs
@@ -0,0 +1,20 @@
+using System;
+
+using Mindscape.LightSpeed;
+using Mindscape.LightSpeed.Validation;
+using Mindscape.LightSpeed.Linq;
+
+namespace SharpTwit
+{
+
+
+  /// <summary>
+  /// Provides a strong-typed unit of work for working with the SharpTwit model.
+  /// </summary>
+  [System.CodeDom.Compiler.GeneratedCode("LightSpeedModelGenerator", "1.0.0.0")]
+  internal partial class SharpTwitUnitOfWork : Mindscape.LightSpeed.UnitOfWork
+  {
+
+  }
+
+}
