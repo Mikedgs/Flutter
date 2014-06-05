b3e6fffb92eb7645575082e1e3a82d8e7c6ea2d7
Ninjah45 <Ben.tehira@gmail.com>
2014-06-05 17:04:20 +1200

    Done some of the Home page

--- SharpTwit/Models/HomeViewModel.cs
+++ SharpTwit/Models/HomeViewModel.cs
@@ -10,21 +10,38 @@ namespace Flutter.Models
     {
         public Repository<Tweet> repoTweet = new Repository<Tweet>();
         public IEnumerable<Tweet> ListOfTweets { get; set; }
+        public Repository<User> repoUser = new Repository<User>();
+        public IEnumerable<User> ListOfUsers { get; set; }
 
         public HomeViewModel()
         {
             ListOfTweets = GetAllTweet();
+            ListOfUsers = GetAllUsers();
         }
 
         public IEnumerable<Tweet> GetAllTweet()
         {
             List<Tweet> top2 = new List<Tweet>();
-            var tweetList = repoTweet.GetAll().OrderBy(x => x.UpdatedAt);
+            var tweetList = repoTweet.GetAll().OrderByDescending(x => x.UpdatedAt);
             if (tweetList.Count() > 1)
             {
                 for (int i = 0; i < 2; i++)
                 {
                     top2.Add(tweetList.ElementAt(i));
+                }
+            }
+            return top2;
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            List<User> top2 = new List<User>();
+            var userList = repoUser.GetAll().OrderByDescending(x => x.UpdatedAt);
+            if (userList.Count() > 1)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    top2.Add(userList.ElementAt(i));
                 }
             }
             return top2;
