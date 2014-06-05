b807dea26e4a377b2c8d40f4934c69eaea2b575d
Ninjah45 <Ben.tehira@gmail.com>
2014-06-05 17:03:04 +1200

    Added followers and following

---
+++ FlutterTests/TweetTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SharpTwit;
+using Flutter;
+using Flutter.Controllers;
+
+//namespace FlutterTests
+//{
+//    [TestFixture]
+//    public class TweetTests
+//    {
+//        [Test]
+//        public void that_created_a_new_tweet_correctly_saves_to_the_database()
+//        {
+//            TweetController classUnderTest = new TweetController();
+//            Tweet tweet = new Tweet() { };
+//            classUnderTest.Tweet(tweet);
+//        }
+//    }
+//}
